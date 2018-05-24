caf6cad599a951c0bd0b59c2e82e83a98ef95e00
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-05-24 23:13:01 +0100

    changes

--- js/catalogo.js
+++ js/catalogo.js
@@ -31,4 +31,38 @@ window.onload = function () {
     btnMenu()
     //fim aparência
 
+    let btnPainelAdmin = document.getElementById("btnPainelAdmin")
+
+    if (idUtilizadorLogado !== -1) {
+        if (Utilizador.getTipoAcessoById(idUtilizadorLogado) !== 2) {
+            btnPainelAdmin.style.display = "inline"
+        } else {
+            btnPainelAdmin.style.display = "none"
+        }
+
+        //btn logout
+        let btnLogout = document.getElementById("btnLogout")
+        btnLogout.addEventListener("click", function () {
+            swal("Até à próxima, " + Utilizador.getPrimeiroNomeById(idUtilizadorLogado) + "!", {
+                icon: "success",
+                buttons: false,
+                closeOnClickOutside: false,
+                closeOnEsc: false,
+                timer: 1000,
+            });
+
+            idUtilizadorLogado = -1
+            localStorage.setItem("idUtilizadorLogado", idUtilizadorLogado)
+            btnPainelAdmin.style.display = "none"
+            areaUtilizador.style.display = "none"
+            btnLogin.style.display = "inline"
+            setTimeout(function () {
+                window.location.href = '../index.html'
+            }, 1000)
+        })
+    } else {
+        document.getElementsByTagName("body")[0].innerHTML = ""
+        window.location.href = '../index.html'
+    }
+
 } //fim onload
