b42cea6a8197fa3398bd60989d2fde6f6b89db4c
Gustavo Vasconcelos <gustavofamoso48@gmail.com>
2018-05-28 16:46:40 +0100

    changes

--- js/catalogo-genero.js
+++ js/catalogo-genero.js
@@ -80,6 +80,7 @@ window.onload = function () {
 
     gerarLivros(idGeneroClicado)
 
+    let disposicao = "grelha"    
     //alterar filtro
     let filtros = document.getElementsByTagName("label")
     let filtroSelecionado = "filtroRelevancia"
@@ -87,16 +88,32 @@ window.onload = function () {
         filtros[i].addEventListener("click", function () {
             let nomeFiltro = filtros[i].getAttribute("for")
             if (filtroSelecionado !== nomeFiltro) {
-                gerarLivros(idGeneroClicado, nomeFiltro)
+                gerarLivros(idGeneroClicado, nomeFiltro, disposicao)
                 filtroSelecionado = nomeFiltro
             }
         })
     }
 
+    //mudar grelha
+    let btnGrelha = document.getElementById("btnGrelha")
+    let btnLista = document.getElementById("btnLista")
+    btnGrelha.addEventListener("click", function () {
+        btnLista.classList.remove("btnAtivo")
+        btnGrelha.classList.add("btnAtivo")
+        disposicao = "grelha"
+        gerarLivros(idGeneroClicado, filtroSelecionado, disposicao)
+    })
+    btnLista.addEventListener("click", function () {
+        btnGrelha.classList.remove("btnAtivo")
+        btnLista.classList.add("btnAtivo")
+        disposicao = "lista"
+        gerarLivros(idGeneroClicado, filtroSelecionado, disposicao)
+    })
+
 
 } //fim onload
 
-function gerarLivros(idGenero, filtro = "filtroRelevancia") {
+function gerarLivros(idGenero, filtro = "filtroRelevancia", disposicao = "grelha") {
     document.getElementById("genero").innerHTML = Genero.getNomeById(idGenero)
 
     switch (filtro) {
@@ -125,28 +142,53 @@ function gerarLivros(idGenero, filtro = "filtroRelevancia") {
             console.log(`Erro: filtro inválido (${filtro})!`)
             break;
     }
-    
+
     let str = `<div class="row mt-1 d-flex justify-content-start text-center">`
     for (let i in livros) {
         if (livros[i].idGenero === idGenero) {
-            str += `<div class="col-xl-4 col-lg-5 col-sm-6 col-10 mt-4 livro-recente">
-                        <figure>
-                            <div class="livro-card">
-                                <a href="livro.html" class="clicarLivro" id="livro${livros[i].id}"><img class="img-fluid" src="${livros[i].urlCapa}"></a>
+            if (disposicao === "grelha") {
+                str += `<div class="col-xl-4 col-lg-5 col-sm-6 col-10 mt-4 livro-recente">
+                            <figure>
+                                <div class="livro-card">
+                                    <a href="livro.html" class="clicarLivro" id="livro${livros[i].id}"><img class="img-fluid" src="${livros[i].urlCapa}"></a>
+                                </div>
+                                <figcaption class="px-2">
+                                    <div>
+                                        <a href="livro.html" class="livro-titulo clicarLivro" id="livro${livros[i].id}">${livros[i].titulo}</a>
+                                    </div>
+                                    <div class="livro-autor">
+                                        ${livros[i].autor.join(", ")}
+                                    </div>
+                                </figcaption>
+                            </figure>
+                        </div>`
+            }
+            if (disposicao === "lista") {
+                let descricao = (livros[i].descricao.length > 200) ? livros[i].descricao.substr(0, livros[i].descricao.indexOf(" ", 200)) + "..." : livros[i].descricao
+                str += `<div class="row">
+                            <div class="col-xl-4 col-lg-5 col-md-6 col-sm-7 col-20 pull-left livro-recente text-center">
+                                <figure id="livro${livros[i].id}">
+                                    <div class="livro-card">
+                                        <a href="livro.html" class="clicarLivro" id="livro${livros[i].id}"><img class="img-fluid" src="${livros[i].urlCapa}"></a>
+                                    </div>
+                                </figure>
                             </div>
-                            <figcaption class="px-2">
-                                <div>
-                                    <a href="livro.html" class="livro-titulo clicarLivro" id="livro${livros[i].id}">${livros[i].titulo}</a>
-                                </div>
-                                <div class="livro-autor">
-                                    ${livros[i].autor.join(", ")}
-                                </div>
-                            </figcaption>
-                        </figure>
-                    </div>`
+                            <div class="col-xl-13 col-lg-15 col-md-14 col-sm-13 col-20 text-white text-left">
+                                <a href="livro.html" class="clicarLivro" id="livro${livros[i].id}"><h4 class="livro-titulo">${livros[i].titulo}</h4></a>
+                                <p style="font-size: .9em;">de ${livros[i].autor.join(", ")}</p>
+                                <p>${descricao}</p>
+                            </div>
+                        </div>`
+            }
+
         }
     }
     str += "</div>"
     document.getElementById("recentes").innerHTML = str
     livroClicado()
 }
+
+
+let a = "Quando Eddard Stark, lorde do castelo de Winterfell, recebe a visita do velho amigo, o rei Robert Baratheon, está longe de adivinhar que a sua vida, e a da sua família, está prestes a entrar numa espiral de tragédia, conspiração e morte. Durante a estadia, o rei convida Eddard a mudar-se para a corte e a assumir a prestigiada posição de Mão do Rei. Este aceita, mas apenas porque desconfia que o anterior detentor desse título foi envenenado pela própria rainha: uma cruel manipuladora do clã Lannister. Assim, perto do rei, Eddard tem esperança de o proteger da rainha. Mas ter os Lannister como inimigos é fatal: a ambição dessa família não tem limites e o rei corre um perigo muito maior do que Eddard temia! Sozinho na corte, Eddard também se apercebe que a sua vida nada vale. E até a sua família, longe no norte, pode estar em perigo. Uma galeria de personagens brilhantes dá vida a esta saga: o anão Tyrion, ovelha negra do clã Lannister; Jon Snow, bastardo de Eddard Stark que decide juntar-se à Patrulha da Noite, e a princesa Daenerys Targaryen, da dinastia que reinou antes de Robert, que pretende ressuscitar os dragões do passado para recuperar o trono, custe o que custar."
+
+a = (a.length > 200) ? a.substr(0, a.indexOf(" ", 200)) + "..." : a
