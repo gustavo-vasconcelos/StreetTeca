c062ddde6795037e025dcee6e1343cf91204a330
GUSTAVO HENRIQUE DA SILVA VASCONCELOS <9170196@eseig.pt>
2018-06-13 12:33:54 +0100

    changes

--- js/perfil.js
+++ js/perfil.js
@@ -82,6 +82,10 @@ function gerarInfo() {
             str += `<div class="col-lg-5 col-md-8 col-sm-20 col-20 mt-2 text-center">
                         <div class="foto-testemunho text-center">
                             <img class="img-thumbnail" src="../${utilizadores[i].urlFoto}">
+                            <div class="text-center">
+                                Multa: €${utilizadores[i].multa}
+                            </div>
+                            
                             <div class="row px-5">
                                 <button type="button" class="col-20 btn btn-teca3 mt-2" style="border-radius: 2em;" id="btnEditarPerfil" data-toggle="modal" data-target="#modal">
                                 <i class="fa fa-pencil text-teca4"></i> Editar o perfil
@@ -129,6 +133,7 @@ function gerarInfo() {
     document.getElementById("btnEditarPerfil").addEventListener("click", function () {
         modalTitulo.innerHTML = "A editar perfil"
         for (let i in utilizadores) {
+            console.log(idUtilizadorLogado)
             if (utilizadores[i].id === idUtilizadorLogado) {
                 let urlFoto = (utilizadores[i].urlFoto === "img/perfil.png") ? "" : urlFoto
                 modalBody.innerHTML = ` <div class="container-fluid">
@@ -178,11 +183,14 @@ function gerarInfo() {
                                                                 <i class="fa fa-info" aria-hidden="true"></i>
                                                             </span>
                                                         </div>
-                                                        <textarea id="inputEditarBiografia" class="form-control" rows="5" required style="resize: vertical; min-height: 48px; max-height: 150px;"></textarea>
+                                                        <textarea id="inputEditarBiografia" class="form-control" rows="5" style="resize: vertical; min-height: 48px; max-height: 150px;"></textarea>
                                                     </div>
                                                 </div>
                                                 <div class="text-center">
-                                                    <input type="submit" class="col-10 btn btn-teca3" value="Confirmar">                                                
+                                                    <div>
+                                                        <button type="button" class="col-sm-10 col-13 btn btn-teca3 mt-2" id="btnAlterarPassword"> Alterar password
+                                                    </div>
+                                                    <input type="submit" class="col-sm-10 col-13 btn btn-teca3 mt-2" value="Confirmar">                                                
                                                 </div>
                                             </form>                                  
                                         </div>`
@@ -194,6 +202,81 @@ function gerarInfo() {
                 } else {
                     inputBiografia.innerHTML = "Escreva algo sobre si..."
                 }
+
+                document.getElementById("formEditar").addEventListener("submit", function (event) {
+                    let inputEditarNome = document.getElementById("inputEditarNome")
+                    let inputEditarEmail = document.getElementById("inputEditarEmail")
+                    let inputEditarUrlFoto = document.getElementById("inputEditarUrlFoto")
+                    let inputEditarBiografia = document.getElementById("inputEditarBiografia")
+                    utilizadores[i].nome = inputEditarNome.value
+                    utilizadores[i].email = inputEditarEmail.value
+                    utilizadores[i].urlFoto = inputEditarUrlFoto.value
+                    utilizadores[i].biografia = inputEditarBiografia.value
+
+                    //atualizar key
+                    localStorage.setItem("utilizadores", JSON.stringify(utilizadores))
+
+                    $("#modal").modal("hide")
+                    swal("Perfil editado.", "", "success")
+                    gerarInfo()
+
+                    event.preventDefault()
+                })
+
+                document.getElementById("btnAlterarPassword").addEventListener("click", function () {
+                    modalBody.innerHTML = `<form class="form-horizontal" id="formEditar">
+                                                <div class="form-group">
+                                                    <label for="inputEditarNome">Nome *</label>
+                                                    <div class="input-group">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text">
+                                                                <i class="fa fa-id-card" aria-hidden="true"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input required autofocus type="text" class="form-control" id="inputEditarNome" value="${utilizadores[i].nome}">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputEditarEmail">Email *</label>
+                                                    <div class="input-group">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text">
+                                                                <i class="fa fa-at" aria-hidden="true"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input required autofocus type="text" class="form-control" id="inputEditarEmail" value="${utilizadores[i].email}">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputEditarUrlFoto">URL foto</label>
+                                                    <div class="input-group">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text">
+                                                                <i class="fa fa-link" aria-hidden="true"></i>
+                                                            </span>
+                                                        </div>
+                                                        <input autofocus type="text" class="form-control" id="inputEditarUrlFoto" value="${urlFoto}">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group">
+                                                    <label for="inputEditarBiografia">Biografia</label>
+                                                    <div class="input-group">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text">
+                                                                <i class="fa fa-info" aria-hidden="true"></i>
+                                                            </span>
+                                                        </div>
+                                                        <textarea id="inputEditarBiografia" class="form-control" rows="5" style="resize: vertical; min-height: 48px; max-height: 150px;"></textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="text-center">
+                                                    <div>
+                                                        <button type="button" class="col-sm-10 col-13 btn btn-teca3 mt-2" id="btnAlterarPassword"> Alterar password
+                                                    </div>
+                                                    <input type="submit" class="col-sm-10 col-13 btn btn-teca3 mt-2" value="Confirmar">                                                
+                                                </div>
+                                            </form>`
+                })
             }
         }
         modalFooter.innerHTML = ""
